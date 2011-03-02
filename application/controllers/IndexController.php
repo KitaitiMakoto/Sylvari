05376969df705b03ee042b694b15a4f04d33b7bd
KitaitiMakoto <KitaitiMakoto@gmail.com>
2011-03-03 00:09:36 +0900

    add login/logout form

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,14 +5,10 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
     }
 
     public function indexAction()
     {
-        // action body
     }
-
-
 }
 
