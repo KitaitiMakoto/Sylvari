378a1bdbae968f6a831a8b3c673cf46d36e16d94
KitaitiMakoto <KitaitiMakoto@gmail.com>
2011-03-02 23:58:08 +0900

    fix a bug

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -7,7 +7,7 @@ class Form_Login extends Zend_Form
     public function init()
     {
         $this->setMethod('post')
-             ->setAttrib('user-id', 'login')
+             ->setAttrib('id', 'login')
              ->addElement('text', 'userid', array(
                    'label' => 'Login ID:',
                    'required' => true
