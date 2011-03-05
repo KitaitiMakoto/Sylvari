1653257cece1c620648967fe68b998100c4ee6ef
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-03-06 03:53:48 +0900

    add a setting of autoloading

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,14 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    protected function _initAutoload()
+    {
+        $moduleLoader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => '',
+            'basePath' => APPLICATION_PATH,
+        ));
+        return $moduleLoader;
+    }
 
 }
 
