6b1754cf8b757483bf84d02ab3e8c64e9920df7c
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-03-19 20:56:10 +0900

    AuthorTest: add a negative test about addBook method

--- tests/application/models/AuthorTest.php
+++ tests/application/models/AuthorTest.php
@@ -1,6 +1,7 @@
 <?
 
 require_once dirname(__FILE__) . '/../../test_helper.php';
+require_once APPLICATION_PATH . '/models/Book.php';
 require_once APPLICATION_PATH . '/models/Author.php';
 
 class AuthorTest extends PHPUnit_Framework_TestCase
@@ -27,13 +28,21 @@ class AuthorTest extends PHPUnit_Framework_TestCase
     
     public function testHasSomeBooksWhenCountedAsAuthorOfSomeBooks()
     {
-        $book1 = new stdClass();
-        $book2 = new stdClass();
+        $book1 = $this->getMock('Model_Book');
+        $book2 = $this->getMock('Model_Book');
         
         foreach (array($book1, $book2) as $book) {
             $this->author->addBook($book);
         }
         
-        $this->assertEquals(array($book1, $book2), $this->getBooks());
+        $this->assertEquals(array($book1, $book2), $this->author->getBooks());
+    }
+    
+    public function testUnableToAddNonBookObject()
+    {
+        $nonBook = new stdClass;
+        
+        $this->setExpectedException('PHPUnit_Framework_Error');
+        $this->author->addBook($nonBook);
     }
 }
